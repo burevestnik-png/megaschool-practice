b74772c1c06db24bf2315977492c4c35987cc67e
timermakov <ts.ermakov@yandex.ru>
2024-02-17 13:18:52 +0300

    Add "Next" button and ImageView, update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "ru.sample.duckapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ru.sample.duckapp"
@@ -37,12 +37,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/ru/sample/duckapp/data/DucksApi.kt
+++ app/src/main/java/ru/sample/duckapp/data/DucksApi.kt
@@ -1,8 +1,11 @@
 package ru.sample.duckapp.data
 
+import android.widget.ImageView
 import retrofit2.Call
 import retrofit2.http.GET
 import ru.sample.duckapp.domain.Duck
+import kotlin.random.Random
+
 
 interface DucksApi {
     @GET("random")
